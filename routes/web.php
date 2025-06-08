63d5fd05c1145eaf5b1ce25e3dd0cdc5f390e014
RajputNiraj <rajputniraj167@gmail.com>
2025-06-08 14:05:53 +0530

    migration and model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Sample1Controller;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource("sample1", Sample1Controller::class);
