42e4e2cbd2bd34db149ea3bf299579675186bc6e
RajputNiraj <rajputniraj167@gmail.com>
2025-06-08 23:12:17 +0530

    seed data via factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Blog;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +19,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            "id" => 1,
+            "password" => bcrypt("12345678")
         ]);
+
+        // Blogs Data
+        Blog::factory(100)->create();
     }
 }
